4ab0e67cfb20007d6eadda994f13f0812f28b6e2
RAVIRAUSHANVISHWAKARMA <raviraushanvishwakarma2004@gmail.com>
2025-01-25 10:42:20 +0530

    Portfolio done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Nav, Logo, NavLink, Bars, NavMenu, NavBtn } from "./HeaderElements";
+
+const Header = ({ toggle }) => {
+  return (
+    <div className="Container" style={{padding: 0}}>
+      <Nav>
+        <Logo to="/">
+          <img
+            src="/R-LOGO.png"
+            alt="logo"
+          />
+        </Logo>
+        <NavMenu>
+          <NavLink className="menu-item" to="projects">
+            Projects
+          </NavLink>
+          <NavLink className="menu-item" to="about">
+            About
+          </NavLink>
+          <NavLink className="menu-item" to="contact">
+            Contact
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <a
+            className="btn PrimaryBtn"
+            href="https://www.linkedin.com/in/ravi-raushan-vishwakarma-2a5086252/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Resume
+          </a>
+        </NavBtn>
+        <Bars onClick={toggle} />
+      </Nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,137 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/Crop-Cartel-responsiv.png",
+    title: "Crop Cartel",
+    description:
+      "Crop Cartel is an online platform connecting farmers and customers through a bidding system for buying and selling agricultural products efficiently..",
+    tech_stack: ["HTML", "CSS", "JavaScript"],
+    github_url: "https://github.com/RAVIRAUSHANVISHWAKARMA/Crop-cartel.git",
+    demo_url: "https://cropcartel.onrender.com/",
+  },
+  {
+    img: "/Ayush-responsive.png",
+    title: "Ayush Portal",
+    description:
+      "Ayush Portal connects job seekers, investors, and entrepreneurs in the medical field with dynamic data handling and role-based access.",
+    tech_stack: [ "HTML", "CSS","React", "Node js", "Postgrey SQL"],
+    github_url: "https://github.com/RAVIRAUSHANVISHWAKARMA/Ayush-Portal.git",
+    demo_url: "https://ayush-portal-wcld.onrender.com/",
+  },
+  {
+    img: "/mindease-responsive.png",
+    title: "Mind Ease",
+    description:
+      "An app where users can purchase online therapy sessions.",
+    tech_stack: ["HTML", "CSS", "NodeJs", "MongoDB", "SocketIo"],
+    github_url: "https://github.com/RAVIRAUSHANVISHWAKARMA/Mind-Ease",
+    demo_url: "https://mind-ease.onrender.com/",
+  },
+  {
+    img: "/crypto-reasponsive.png",
+    title: "Crypto-Curious",
+    description:
+      "Developed comprehensive insights and tools for navigating the dynamic cryptocurrency market, empowering users with real-time data and analysis.",
+    tech_stack: ["ReactJs", "Tailwind CSS"],
+    github_url: "https://github.com/RAVIRAUSHANVISHWAKARMA/Crypto-Curious.git",
+    demo_url: "https://crypto-curious.vercel.app/",
+  },
+  {
+    img: "/trip-planner.jpg",
+    title: "Trip-Tracker",
+    description:
+      "This program simulates a travel planner for Delhi using a graph-based approach to optimize routes, traffic analysis, and activity scheduling.",
+    tech_stack: ["c++"],
+    github_url: "https://github.com/RAVIRAUSHANVISHWAKARMA/Trip-Tracker.git",
+    demo_url: "",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/jquery/jquery-original.svg",
+    name: "Jquery",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+  //   name: "Typescript",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg",
+    name: "Github",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+  //   name: "Firebase",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg",
+    name: "C++",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original.svg",
+    name: "Postgrey SQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://www.vectorlogo.zone/logos/canva/canva-icon.svg",
+    name: "Canva",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg",
+    name: "PHP",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-original.svg",
+    name: "TailwindCSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
